cb7083c7327d7849ac4ca72aa827d7aedbf193c8
lucifer <fortran95@126.com>
2013-01-19 00:08:33 +0800

    Add IP recognization

--- class/token.php
+++ class/token.php
@@ -3,21 +3,23 @@ class token{
     private $signkey;
     private $maxsession;
     public function __construct(){
-        global $_unique_key, $_max_session, $database;
+        global $_unique_key, $_max_session, $database, $_SERVER;
         $this->signkey = $_unique_key;
         $this->db = $database;
         $this->maxsession = $_max_session;
+        $this->userip = $_SERVER['REMOTE_ADDR'];
     }
     public function read($tokenstr){
         $parts = explode(':',$tokenstr);
-        if(count($parts) < 5)
+        if(count($parts) < 6)
             return false;
 
-        if(!is_numeric($parts[2])) return false;
+        if(!is_numeric($parts[0])) return false;
         if( time() - $parts[2] > $this->maxsession ) return false;
+        if( $parts[3] != $this->userip ) return false;
 
-        $tokenstr = "{$parts[0]}:{$parts[1]}:{$parts[2]}:{$parts[3]}";
-        $gotsign = $parts[4];
+        $tokenstr = "{$parts[0]}:{$parts[1]}:{$parts[2]}:{$parts[3]}:{$parts[4]}";
+        $gotsign = $parts[5];
 
         if(!$userInfo = $this->getUserInfo($parts[0]))
             return false;
@@ -43,6 +45,7 @@ class token{
         $tokenstr = implode(':',array($this->userid,
                                       $this->username,
                                       (int)time(),
+                                      $this->userip,
                            ));
         # generate random padding
         $padding = md5(rand() . $tokenstr);
