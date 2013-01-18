1b69300714b3c5fb4226b9aca77d1224e26538a2
lucifer <fortran95@126.com>
2013-01-18 09:39:22 +0800

    add login with hash

--- authenticate.php
+++ authenticate.php
@@ -3,10 +3,31 @@ include(dirname(__FILE__) . "/_general_.php");
 
 $username = isset($_POST['username'])?$_POST['username']:false;
 $password = isset($_POST['password'])?$_POST['password']:false;
+$time = isset($_POST['nowtime'])?$_POST['nowtime']:false;
 
-if($username == 'test' && $password == 'test')
-    $r = new success(array('token'=>'token'));
-else
-    $r = new failure('not authenticated.');
+if(!($username && $password && $time))
+    $r = new failure('unknown data');
+else {
+    $timediff = time() - $time;
+    if(!($timediff >= 0 && $timediff < 30))
+        $r = new failure('login timeout.');
+    else {
+        $ary = array(
+            'test'=>'test',
+        );
+        $findkey = strtolower(trim($username));
+        if(!isset($ary[$findkey]))
+            $r = new failure('unknown username');
+        else {
+            $check = hash_hmac('sha1',$ary[$findkey],"$username$time");
+            if($password != $check){
+                $r = new failure('incorrect password');
+            } else {
+                $r = new success(array('token'=>'mytoken'));
+            }
+        }
+    }
+}
+
 
 if(isset($r)) die($r->getJSON());
