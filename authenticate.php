1c3f783dea33e6cce5f8fed172c45fc77d823221
lucifer <fortran95@126.com>
2013-01-18 08:29:49 +0800

    demo login done

--- authenticate.php
+++ authenticate.php
@@ -1,6 +1,12 @@
 <?
 include(dirname(__FILE__) . "/_general_.php");
 
-$r = new failure('not authenticated.');
+$username = isset($_POST['username'])?$_POST['username']:false;
+$password = isset($_POST['password'])?$_POST['password']:false;
+
+if($username == 'test' && $password == 'test')
+    $r = new success(array('token'=>'token'));
+else
+    $r = new failure('not authenticated.');
 
 if(isset($r)) die($r->getJSON());
